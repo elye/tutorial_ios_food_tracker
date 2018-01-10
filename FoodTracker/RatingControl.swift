fc17951166f7fdc69a5a1579d125c4b92228438c
elye.project <elye.project@gmail.com>
2018-01-10 16:35:48 +1100

    Adding IBinspectable to startSize and starCount

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -12,6 +12,16 @@ import UIKit
 
   //MARK: Properties
   private var ratingButtons = [UIButton]()
+  @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+    didSet {
+      setupButtons()
+    }
+  }
+  @IBInspectable var starCount: Int = 5 {
+    didSet {
+      setupButtons()
+    }
+  }
 
   var rating = 0
   //MARK: Initialization
@@ -33,15 +43,23 @@ import UIKit
 
   //MARK: Private Methods
   private func setupButtons() {
-    for _ in 0..<5 {
+    
+    // clear any existing buttons
+    for button in ratingButtons {
+      removeArrangedSubview(button)
+      button.removeFromSuperview()
+    }
+    ratingButtons.removeAll()
+
+    for _ in 0..<starCount {
       // Create the button
       let button = UIButton()
       button.backgroundColor = UIColor.red
 
       // Add constraints
       button.translatesAutoresizingMaskIntoConstraints = false
-      button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-      button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+      button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+      button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
 
       // Setup the button action
       button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
