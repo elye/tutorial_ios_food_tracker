c129ad35e772d6953cfae171f00eb19f86773267
elye.project <elye.project@gmail.com>
2018-01-10 16:51:53 +1100

    Adding stars asset to the rating custom view

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -51,10 +51,21 @@ import UIKit
     }
     ratingButtons.removeAll()
 
+    // Load Button Images
+    let bundle = Bundle(for: type(of: self))
+    let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+    let emptyStar = UIImage(named:"emptyStar", in: bundle, compatibleWith: self.traitCollection)
+    let highlightedStar = UIImage(named:"highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+
     for _ in 0..<starCount {
       // Create the button
       let button = UIButton()
-      button.backgroundColor = UIColor.red
+
+      // Set the button images
+      button.setImage(emptyStar, for: .normal)
+      button.setImage(filledStar, for: .selected)
+      button.setImage(highlightedStar, for: .highlighted)
+      button.setImage(highlightedStar, for: [.highlighted, .selected])
 
       // Add constraints
       button.translatesAutoresizingMaskIntoConstraints = false
