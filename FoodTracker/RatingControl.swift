2a757874344b6961245692aab7d211828601876c
elye.project <elye.project@gmail.com>
2018-01-10 17:23:40 +1100

    Added accessibility support

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -75,7 +75,7 @@ import UIKit
     let emptyStar = UIImage(named:"emptyStar", in: bundle, compatibleWith: self.traitCollection)
     let highlightedStar = UIImage(named:"highlightedStar", in: bundle, compatibleWith: self.traitCollection)
 
-    for _ in 0..<starCount {
+    for index in 0..<starCount {
       // Create the button
       let button = UIButton()
 
@@ -89,6 +89,9 @@ import UIKit
       button.translatesAutoresizingMaskIntoConstraints = false
       button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
       button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+
+      // Set the accessibility label
+      button.accessibilityLabel = "Set \(index + 1) star rating"
 
       // Setup the button action
       button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
@@ -107,6 +110,29 @@ import UIKit
     for (index, button) in ratingButtons.enumerated() {
       // If the index of a button is less than the rating, that button should be selected.
       button.isSelected = index < rating
+
+      // Set the hint string for the currently selected star
+      let hintString: String?
+      if rating == index + 1 {
+        hintString = "Tap to reset the rating to zero."
+      } else {
+        hintString = nil
+      }
+
+      // Calculate the value string
+      let valueString: String
+      switch (rating) {
+      case 0:
+        valueString = "No rating set."
+      case 1:
+        valueString = "1 star set."
+      default:
+        valueString = "\(rating) stars set."
+      }
+
+      // Assign the hint string and value string
+      button.accessibilityHint = hintString
+      button.accessibilityValue = valueString
     }
   }
 }
