cb60a89cbbf9a53ff0ccfad244088ef795371829
elye.project <elye.project@gmail.com>
2018-01-10 16:02:36 +1100

    Add rating buttons, and @IBDesignable setting to it

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
 
+  //MARK: Properties
+  private var ratingButtons = [UIButton]()
+
+  var rating = 0
   //MARK: Initialization
   override init(frame: CGRect) {
     super.init(frame: frame)
@@ -29,18 +33,24 @@ class RatingControl: UIStackView {
 
   //MARK: Private Methods
   private func setupButtons() {
-    let button = UIButton()
-    button.backgroundColor = UIColor.red
+    for _ in 0..<5 {
+      // Create the button
+      let button = UIButton()
+      button.backgroundColor = UIColor.red
 
-    // Add constraints
-    button.translatesAutoresizingMaskIntoConstraints = false
-    button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-    button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+      // Add constraints
+      button.translatesAutoresizingMaskIntoConstraints = false
+      button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+      button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
 
-    // Setup the button action
-    button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+      // Setup the button action
+      button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
 
-    // Add the button to the stack
-    addArrangedSubview(button)
+      // Add the button to the stack
+      addArrangedSubview(button)
+
+      // Add the new button to the rating button array
+      ratingButtons.append(button)
+    }
   }
 }
