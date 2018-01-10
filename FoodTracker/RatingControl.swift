400650513864bae40e627b704a34b693147acf08
elye.project <elye.project@gmail.com>
2018-01-10 16:59:01 +1100

    Star state could be kept now.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -23,7 +23,12 @@ import UIKit
     }
   }
 
-  var rating = 0
+  var rating = 0 {
+    didSet {
+      updateButtonSelectionStates()
+    }
+  }
+
   //MARK: Initialization
   override init(frame: CGRect) {
     super.init(frame: frame)
@@ -38,7 +43,20 @@ import UIKit
   //MARK: Button Action
   @objc
   func ratingButtonTapped(button: UIButton) {
-    print("Button pressed 👍")
+    guard let index = ratingButtons.index(of: button) else {
+      fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+    }
+
+    // Calculate the rating of the selected button
+    let selectedRating = index + 1
+
+    if selectedRating == rating {
+      // If the selected star represents the current rating, reset the rating to 0.
+      rating = 0
+    } else {
+      // Otherwise set the rating to the selected star
+      rating = selectedRating
+    }
   }
 
   //MARK: Private Methods
@@ -80,6 +98,15 @@ import UIKit
 
       // Add the new button to the rating button array
       ratingButtons.append(button)
+    }
+
+    updateButtonSelectionStates()
+  }
+
+  private func updateButtonSelectionStates() {
+    for (index, button) in ratingButtons.enumerated() {
+      // If the index of a button is less than the rating, that button should be selected.
+      button.isSelected = index < rating
     }
   }
 }
