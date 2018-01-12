834bf6bdb48cae6a94076cb682060e4a03d83faa
elye.project <elye.project@gmail.com>
2018-01-13 10:35:10 +1100

    Ability to delete to the meal (and also cancel from the Meal Detail view)

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -18,6 +18,9 @@ class MealTableViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    // Use the edit button item provided by the table view controller.
+    navigationItem.leftBarButtonItem = editButtonItem
+
     // Load the sample data.
     loadSampleMeals()
   }
@@ -63,17 +66,16 @@ class MealTableViewController: UITableViewController {
    }
    */
 
-  /*
-   // Override to support editing the table view.
-   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-   if editingStyle == .delete {
-   // Delete the row from the data source
-   tableView.deleteRows(at: [indexPath], with: .fade)
-   } else if editingStyle == .insert {
-   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-   }
-   }
-   */
+  // Override to support editing the table view.
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      // Delete the row from the data source
+      meals.remove(at: indexPath.row)
+      tableView.deleteRows(at: [indexPath], with: .fade)
+    } else if editingStyle == .insert {
+      // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+  }
 
   /*
    // Override to support rearranging the table view.
