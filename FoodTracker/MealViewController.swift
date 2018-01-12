61eaec2a9e2214e19ad673a9f4a5aa53dafb9490
elye.project <elye.project@gmail.com>
2018-01-12 21:05:51 +1100

    Ability to save a new meal

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import os.log
 
 class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -14,6 +15,13 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
   @IBOutlet weak var nameTextField: UITextField!
   @IBOutlet weak var photoImageView: UIImageView!
   @IBOutlet weak var ratingControl: RatingControl!
+  @IBOutlet weak var saveButton: UIBarButtonItem!
+
+  /*
+   This value is either passed by `MealTableViewController` in `prepare(for:sender:)`
+   or constructed as part of adding a new meal.
+   */
+  var meal: Meal?
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -56,6 +64,27 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
   func textFieldDidEndEditing(_ textField: UITextField) {
   }
 
+  //MARK: Navigation
+  // This method lets you configure a view controller before it's presented.
+
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    super.prepare(for: segue, sender: sender)
+
+    // Configure the destination view controller only when the save button is pressed.
+    guard let button = sender as? UIBarButtonItem, button === saveButton else {
+      os_log("The save button was not pressed, cancelling", log: OSLog.default, type: .debug)
+      return
+    }
+
+    let name = nameTextField.text ?? ""
+    let photo = photoImageView.image
+    let rating = ratingControl.rating
+
+    // Set the meal to be passed to MealTableViewController after the unwind segue.
+    meal = Meal(name: name, photo: photo, rating: rating)
+  }
+  
   // Mark: Actions
   @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
     // Hide the keyboard.
